61172781116a2b80da276e07250aafb1b3042175
Ibi-san <ibragim_nasirov@mail.ru>
2024-05-06 20:00:44 +0300

    ClashRoyale 2.3

---
+++ Client/ClashRoyale/Assets/Scripts/UnitAnimation.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UnitAnimation : MonoBehaviour
+{
+    private const string State = "State";
+    private const string AttackSpeed = "AttackSpeed";
+    [SerializeField] private Animator _animator;
+
+    public void Init(Unit unit)
+    {
+        float damageDelay = unit.parameters.damageDelay;
+        _animator.SetFloat(AttackSpeed, 1 / damageDelay);
+    }
+    
+    public void SetState(UnitStateType stateType) => _animator.SetInteger(State, (int)stateType);
+}
